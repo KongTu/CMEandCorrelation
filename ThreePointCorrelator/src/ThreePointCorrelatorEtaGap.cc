86c910fad998a749af094256c215f8b50659f54e
KongTu <kongkong@pb-d-128-141-112-10.cern.ch>
2016-02-22 16:52:51 +0100

    fix code

--- ThreePointCorrelator/src/ThreePointCorrelatorEtaGap.cc
+++ ThreePointCorrelator/src/ThreePointCorrelatorEtaGap.cc
@@ -490,7 +490,7 @@ ThreePointCorrelatorEtaGap::beginJob()
 
   Ntrk = fs->make<TH1D>("Ntrk",";Ntrk",400,0,400);
   evtWeight = fs->make<TH1D>("evtWeight",";evtWeight", 100000,0,100000);
-  evtWeightedQp3 = fs->make<TH1D>("evtWeightedQp3",";evtWeightedQp3", 100000,0,5000);
+  evtWeightedQp3 = fs->make<TH1D>("evtWeightedQp3",";evtWeightedQp3", 10000,0,50);
   averageCos = fs->make<TH1D>("averageCos",";averageCos", 200,-1,1);
   averageSin = fs->make<TH1D>("averageSin",";averageSin", 200,-1,1);
 
