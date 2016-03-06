d6f4747a1a6e7cec8e43238921a2096c567f4c31
Kong <kongPro@nat-tvwna-inside-visitornet101-c-30479.princeton.org>
2016-03-06 12:32:12 -0500

    fix code

--- ThreePointCorrelator/src/ThreePointCorrelatorEtaGap.cc
+++ ThreePointCorrelator/src/ThreePointCorrelatorEtaGap.cc
@@ -620,10 +620,10 @@ ThreePointCorrelatorEtaGap::beginJob()
 
   for(int type = 0; type < 3; type++){
     
-    QvsdEtaPlusPlus[type] = fs->make<TH2D>(Form("QvsdEtaPlusPlus_%d", type),";#Delta#eta;Q_{#phi_{1,+}}Q_{#phi_{2,+}}Q^{*}_{2#phi_{3}}", bins, dEtaBinsArray, 4000000,-0.2,0.2 );
-    QvsdEtaMinusMinus[type] = fs->make<TH2D>(Form("QvsdEtaMinusMinus_%d", type),";#Delta#eta;Q_{#phi_{1,-}}Q_{#phi_{2,-}}Q^{*}_{2#phi_{3}}", bins, dEtaBinsArray, 4000000,-0.2,0.2 );
-    QvsdEtaPlusMinus[type] = fs->make<TH2D>(Form("QvsdEtaPlusMinus_%d", type),";#Delta#eta;Q_{#phi_{1,+}}Q_{#phi_{2,-}}Q^{*}_{2#phi_{3}}", bins, dEtaBinsArray, 4000000,-0.2,0.2 );
-    QvsdEtaMinusPlus[type] = fs->make<TH2D>(Form("QvsdEtaMinusPlus_%d", type),";#Delta#eta;Q_{#phi_{1,-}}Q_{#phi_{2,+}}Q^{*}_{2#phi_{3}}", bins, dEtaBinsArray, 4000000,-0.2,0.2 );
+    QvsdEtaPlusPlus[type] = fs->make<TH2D>(Form("QvsdEtaPlusPlus_%d", type),";#Delta#eta;Q_{#phi_{1,+}}Q_{#phi_{2,+}}Q^{*}_{2#phi_{3}}", bins, dEtaBinsArray, 400000,-0.2,0.2 );
+    QvsdEtaMinusMinus[type] = fs->make<TH2D>(Form("QvsdEtaMinusMinus_%d", type),";#Delta#eta;Q_{#phi_{1,-}}Q_{#phi_{2,-}}Q^{*}_{2#phi_{3}}", bins, dEtaBinsArray, 400000,-0.2,0.2 );
+    QvsdEtaPlusMinus[type] = fs->make<TH2D>(Form("QvsdEtaPlusMinus_%d", type),";#Delta#eta;Q_{#phi_{1,+}}Q_{#phi_{2,-}}Q^{*}_{2#phi_{3}}", bins, dEtaBinsArray, 400000,-0.2,0.2 );
+    QvsdEtaMinusPlus[type] = fs->make<TH2D>(Form("QvsdEtaMinusPlus_%d", type),";#Delta#eta;Q_{#phi_{1,-}}Q_{#phi_{2,+}}Q^{*}_{2#phi_{3}}", bins, dEtaBinsArray, 400000,-0.2,0.2 );
   
   }
 
