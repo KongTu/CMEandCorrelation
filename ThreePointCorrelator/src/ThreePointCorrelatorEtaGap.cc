9766deda177a5095cc4eeea7a6c33b1e534adf41
KongTu <kongkong@pb-d-128-141-112-10.cern.ch>
2016-04-13 13:30:48 +0200

    test

--- ThreePointCorrelator/src/ThreePointCorrelatorEtaGap.cc
+++ ThreePointCorrelator/src/ThreePointCorrelatorEtaGap.cc
@@ -154,7 +154,7 @@ ThreePointCorrelatorEtaGap::analyze(const edm::Event& iEvent, const edm::EventSe
         QsinTRK += weight*sin( 2*trk.phi() );
         QcountsTrk += weight;
 
-        cout << "weight: " << endl;
+        cout << "weight: " << weight << endl;
 
         for(int eta = 0; eta < NetaBins; eta++){
           if( trk.eta() > etaBins_[eta] && trk.eta() < etaBins_[eta+1] ){
