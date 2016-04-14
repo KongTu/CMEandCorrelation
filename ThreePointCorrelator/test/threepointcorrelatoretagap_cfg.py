d0df6d47209f9a8827998694379614c0feb00877
KongTu <kongkong@rice.edu>
2016-04-14 10:55:15 +0200

    config update

--- ThreePointCorrelator/test/threepointcorrelatoretagap_cfg.py
+++ ThreePointCorrelator/test/threepointcorrelatoretagap_cfg.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 process = cms.Process("Demo")
 
-#process.Timing = cms.Service("Timing")
+process.SimpleMemoryCheck = cms.Service("SimpleMemoryCheck",ignoreTotal = cms.untracked.int32(1) )
 
 import HLTrigger.HLTfilters.hltHighLevel_cfi
 process.hltHM = HLTrigger.HLTfilters.hltHighLevel_cfi.hltHighLevel.clone()
@@ -33,12 +33,24 @@ process.load("SimGeneral.MixingModule.mixNoPU_cfi")
 process.source = cms.Source("PoolSource",
     # replace 'myfile.root' with the source file you want to use
     fileNames = cms.untracked.vstring(
-'root://xrootd-cms.infn.it//store/user/davidlw/PAHighPt/PA2013_FlowCorr_PromptReco_TrkHM_Gplus_ReTracking_v18/28b2b9cce04ec3f20baeb96fbd2295a8/pPb_HM_1000_1_BPd.root'    
+#'root://xrootd-cms.infn.it//store/user/davidlw/PAHighPt/PA2013_FlowCorr_PromptReco_TrkHM_Gplus_ReTracking_v18/28b2b9cce04ec3f20baeb96fbd2295a8/pPb_HM_1000_1_BPd.root'    
 #'root://xrootd-cms.infn.it//store/user/davidlw/PAHighPt/PA2013_FlowCorr_PromptReco_TrkHM_Gplus_Reverse_ReTracking_v18/28b2b9cce04ec3f20baeb96fbd2295a8/pPb_HM_1000_1_YyR.root'
+#'root://cmsxrootd.fnal.gov//store/user/davidlw/ReggeGribovPartonMCfix_EposLHC_5TeV_pPb/RecoSkim_ReTracking_v4_5M/5cde49c8740ff28f897f533d05a99dbc/pPb_MB_100_2_s6H.root'
+#'root://xrootd-cms.infn.it//store/user/davidlw/Hijing_PPb502_MinimumBias/RecoSkim_ReTracking_v4_10M/5cde49c8740ff28f897f533d05a99dbc/pPb_MB_100_1_yjL.root'
+'file:/afs/cern.ch/work/z/ztu/CME/CMSSW_5_3_20/src/CMEandCorrelation/ThreePointCorrelator/test/pPb_HM_1000_1_BPd.root'
+#'file:/afs/cern.ch/work/z/ztu/CME/CMSSW_5_3_20/src/CMEandCorrelation/ThreePointCorrelator/test/pPb_MB_100_2_s6H.root'
 )
 )
 
 process.load("CMEandCorrelation.ThreePointCorrelator.threepointcorrelatoretagap_cfi")
+
+#define the cuts
+process.ana.Nmin = 185
+process.ana.Nmax = 220
+process.ana.holeLeft = 0.0
+process.ana.holeRight = 1.0
+process.ana.messAcceptance = False
+process.ana.doEffCorrection = True
 
 process.TFileService = cms.Service("TFileService",fileName = cms.string("test.root"))
 process.p = cms.Path(   process.hltHM*
