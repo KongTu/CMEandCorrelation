f8d027fd1e2c80da997421ad0bf95e80009aed9a
Kong <kongPro@Kongs-MacBook-Pro.local>
2016-04-18 22:26:02 +0200

    add eta test

--- ThreePointCorrelator/src/ThreePointCorrelatorEtaTest.cpp
+++ ThreePointCorrelator/src/ThreePointCorrelatorEtaTest.cpp
@@ -234,21 +234,20 @@ ThreePointCorrelatorEtaTest::analyze(const edm::Event& iEvent, const edm::EventS
   }
 
   for(int ieta = 0; ieta < NetaBins; ieta++){
-    if( ieta > etaBins_[ieta] && ieta < etaBins_[ieta+1] ){  
-      for(int HF = 0; HF < HFside; HF++){
-        for(int sign = 0; sign < 2; sign++){
-          if( Q1_count[ieta][sign] == 0.0 || ETT[HF] == 0.0 ) continue;
+    for(int HF = 0; HF < HFside; HF++){
+      for(int sign = 0; sign < 2; sign++){
+        if( Q1_count[ieta][sign] == 0.0 || ETT[HF] == 0.0 ) continue;
 
-            double Q_real = get3RealOverlap(Q1[ieta][sign][0], Q2[ieta][sign][0], Q3[HF][0], Q1[ieta][sign][1], Q2[ieta][sign][1], Q3[HF][1], Q1_count[ieta][sign], ETT[HF] );
-            QvsdEta[ieta][sign][HF]->Fill( Q_real, Q1_count[ieta][sign]*(Q1_count[ieta][sign]-1)*ETT[HF] );
-        
-          } 
-        if( Q1_count[ieta][0] == 0 || Q1_count[ieta][1] == 0 || ETT[HF] == 0.0 ) continue;
+          double Q_real = get3RealOverlap(Q1[ieta][sign][0], Q2[ieta][sign][0], Q3[HF][0], Q1[ieta][sign][1], Q2[ieta][sign][1], Q3[HF][1], Q1_count[ieta][sign], ETT[HF] );
+          QvsdEta[ieta][sign][HF]->Fill( Q_real, Q1_count[ieta][sign]*(Q1_count[ieta][sign]-1)*ETT[HF] );
+      
+        } 
+      if( Q1_count[ieta][0] == 0 || Q1_count[ieta][1] == 0 || ETT[HF] == 0.0 ) continue;
 
-        double Q_real = get3Real(Q1[ieta][0][0]/Q1_count[ieta][0],Q1[ieta][1][0]/Q1_count[ieta][1],Q3[HF][0]/ETT[HF], Q1[ieta][0][1]/Q1_count[ieta][0], Q1[ieta][1][1]/Q1_count[ieta][1], Q3[HF][1]/ETT[HF]);
-        QvsdEta[ieta][2][HF]->Fill( Q_real, Q1_count[ieta][0]*Q1_count[ieta][1]*ETT[HF] );  
-      } 
-    }
+      double Q_real = get3Real(Q1[ieta][0][0]/Q1_count[ieta][0],Q1[ieta][1][0]/Q1_count[ieta][1],Q3[HF][0]/ETT[HF], Q1[ieta][0][1]/Q1_count[ieta][0], Q1[ieta][1][1]/Q1_count[ieta][1], Q3[HF][1]/ETT[HF]);
+      QvsdEta[ieta][2][HF]->Fill( Q_real, Q1_count[ieta][0]*Q1_count[ieta][1]*ETT[HF] );  
+    } 
+    
   }
 
 /*
