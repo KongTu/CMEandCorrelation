4189ed8abc98334e0bf98bcbf9b22412b03305c5
KongTu <kongkong@pb-d-128-141-112-10.cern.ch>
2016-04-19 15:24:41 +0200

    fix weight QaQb

--- ThreePointCorrelator/src/ThreePointCorrelatorEtaGap.cc
+++ ThreePointCorrelator/src/ThreePointCorrelatorEtaGap.cc
@@ -177,9 +177,9 @@ ThreePointCorrelatorEtaGap::analyze(const edm::Event& iEvent, const edm::EventSe
               Q1[eta][0][1] += weight*sin( trk.phi() );
               Q1_count[eta][0] += weight;
 
-              Q2[eta][0][0] += weight*cos( 2*trk.phi() );
-              Q2[eta][0][1] += weight*sin( 2*trk.phi() );
-              Q2_count[eta][0] += weight;
+              Q2[eta][0][0] += weight*weight*cos( 2*trk.phi() );
+              Q2[eta][0][1] += weight*weight*sin( 2*trk.phi() );
+              Q2_count[eta][0] += weight*weight;
 
             }
             else if( trk.charge() == -1){
@@ -187,9 +187,9 @@ ThreePointCorrelatorEtaGap::analyze(const edm::Event& iEvent, const edm::EventSe
               Q1[eta][1][1] += weight*sin( trk.phi() );
               Q1_count[eta][1] += weight;
 
-              Q2[eta][1][0] += weight*cos( 2*trk.phi() );
-              Q2[eta][1][1] += weight*sin( 2*trk.phi() );
-              Q2_count[eta][1] += weight;
+              Q2[eta][1][0] += weight*weight*cos( 2*trk.phi() );
+              Q2[eta][1][1] += weight*weight*sin( 2*trk.phi() );
+              Q2_count[eta][1] += weight*weight;
 
             }
           }
@@ -255,11 +255,12 @@ ThreePointCorrelatorEtaGap::analyze(const edm::Event& iEvent, const edm::EventSe
 
                 if( Q1_count[ieta][sign] == 0.0 || ETT[HF] == 0.0 ) continue;
 
-                double Q_real = get3RealOverlap(Q1[ieta][sign][0], Q2[ieta][sign][0], Q3[HF][0], Q1[ieta][sign][1], Q2[ieta][sign][1], Q3[HF][1], Q1_count[ieta][sign], ETT[HF] );
-                QvsdEta[deta][sign][HF]->Fill( Q_real, Q1_count[ieta][sign]*(Q1_count[ieta][sign]-1)*ETT[HF] );
+                double Q_real = get3RealOverlap(Q1[ieta][sign][0], Q2[ieta][sign][0], Q3[HF][0], Q1[ieta][sign][1], Q2[ieta][sign][1], Q3[HF][1], Q1_count[ieta][sign], Q2_count[ieta][sign], ETT[HF] );
+                QvsdEta[deta][sign][HF]->Fill( Q_real, (Q1_count[ieta][sign]*Q1_count[ieta][sign] - Q2_count[ieta][sign])*ETT[HF] );
                 
               }
 
+              if( Q1_count[ieta][0] == 0 || Q1_count[ieta][1] == 0 ) cout << "zero count for either positive or negative!" << endl;
               if( Q1_count[ieta][0] == 0 || Q1_count[ieta][1] == 0 || ETT[HF] == 0.0 ) continue;
 
               double Q_real = get3Real(Q1[ieta][0][0]/Q1_count[ieta][0],Q1[jeta][1][0]/Q1_count[jeta][1],Q3[HF][0]/ETT[HF], Q1[ieta][0][1]/Q1_count[ieta][0], Q1[jeta][1][1]/Q1_count[jeta][1], Q3[HF][1]/ETT[HF]);
@@ -367,11 +368,11 @@ ThreePointCorrelatorEtaGap::get3Real(double R1, double R2, double R3, double I1,
   return t1-t2-t3-t4;
 }
 
-double ThreePointCorrelatorEtaGap::get3RealOverlap(double R1, double R2, double R3, double I1, double I2, double I3, double N1, double N3){
+double ThreePointCorrelatorEtaGap::get3RealOverlap(double R1, double R2, double R3, double I1, double I2, double I3, double N1, double N2, double N3){
 
       double t1 = (R1*R1 - I1*I1 - R2)*R3;
       double t2 = (2*R1*I1-I2)*I3;
-      double N = N1*(N1-1)*N3;
+      double N = (N1*N1-N2)*N3;
 
       if( N == 0.0 ){return 0.0;}
       else{return (t1-t2)/N;}
