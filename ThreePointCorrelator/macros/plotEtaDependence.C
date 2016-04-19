95ffd07598eecc1670236f57b190c06e5d45c0c0
KongTu <kongkong@pb-d-128-141-112-10.cern.ch>
2016-04-19 11:46:03 +0200

    add macros

---
+++ ThreePointCorrelator/macros/plotEtaDependence.C
@@ -0,0 +1,173 @@
+#include "RiceStyle.h"
+
+using namespace std;
+
+const int MAXTRACKS = 40000;
+double etabins[] = {-2.4, -2.38, -2.36, -2.34, -2.32, -2.3, -2.28, -2.26, -2.24, -2.22, -2.2, -2.18, -2.16, -2.14, -2.12, -2.1, -2.08, -2.06, -2.04, -2.02, -2, -1.98, -1.96, -1.94, -1.92, -1.9, -1.88, -1.86, -1.84, -1.82, -1.8, -1.78, -1.76, -1.74, -1.72, -1.7, -1.68, -1.66, -1.64, -1.62, -1.6, -1.58, -1.56, -1.54, -1.52, -1.5, -1.48, -1.46, -1.44, -1.42, -1.4, -1.38, -1.36, -1.34, -1.32, -1.3, -1.28, -1.26, -1.24, -1.22, -1.2, -1.18, -1.16, -1.14, -1.12, -1.1, -1.08, -1.06, -1.04, -1.02, -1, -0.98, -0.96, -0.94, -0.92, -0.9, -0.88, -0.86, -0.84, -0.82, -0.8, -0.78, -0.76, -0.74, -0.72, -0.7, -0.68, -0.66, -0.64, -0.62, -0.6, -0.58, -0.56, -0.54, -0.52, -0.5, -0.48, -0.46, -0.44, -0.42, -0.4, -0.38, -0.36, -0.34, -0.32, -0.3, -0.28, -0.26, -0.24, -0.22, -0.2, -0.18, -0.16, -0.14, -0.12, -0.1, -0.08, -0.06, -0.04, -0.02, 1.77636e-15, 0.02, 0.04, 0.06, 0.08, 0.1, 0.12, 0.14, 0.16, 0.18, 0.2, 0.22, 0.24, 0.26, 0.28, 0.3, 0.32, 0.34, 0.36, 0.38, 0.4, 0.42, 0.44, 0.46, 0.48, 0.5, 0.52, 0.54, 0.56, 0.58, 0.6, 0.62, 0.64, 0.66, 0.68, 0.7, 0.72, 0.74, 0.76, 0.78, 0.8, 0.82, 0.84, 0.86, 0.88, 0.9, 0.92, 0.94, 0.96, 0.98, 1, 1.02, 1.04, 1.06, 1.08, 1.1, 1.12, 1.14, 1.16, 1.18, 1.2, 1.22, 1.24, 1.26, 1.28, 1.3, 1.32, 1.34, 1.36, 1.38, 1.4, 1.42, 1.44, 1.46, 1.48, 1.5, 1.52, 1.54, 1.56, 1.58, 1.6, 1.62, 1.64, 1.66, 1.68, 1.7, 1.72, 1.74, 1.76, 1.78, 1.8, 1.82, 1.84, 1.86, 1.88, 1.9, 1.92, 1.94, 1.96, 1.98, 2, 2.02, 2.04, 2.06, 2.08, 2.1, 2.12, 2.14, 2.16, 2.18, 2.2, 2.22, 2.24, 2.26, 2.28, 2.3, 2.32, 2.34, 2.36, 2.38, 2.4};
+//double etabins[] = {-2.4,-2.3,-2.2,-2.1,-2,-1.9,-1.8,-1.7,-1.6,-1.5,-1.4,-1.3,-1.2,-1.1,-1,-0.9,-0.8,-0.7,-0.6,-0.5,-0.4,-0.3,-0.2,-0.1,0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1,1.1,1.2,1.3,1.4,1.5,1.6,1.7,1.8,1.9,2,2.1,2.2,2.3,2.4};
+const int Nbins = sizeof(etabins) / sizeof(etabins[0]) - 1;
+double dEtaBins[] = {0.0,0.02,0.04,0.06,0.08,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0,1.1,1.2,1.3,1.4,1.5,1.6,1.7,1.8,1.9,2.0,2.2,2.4,2.6,2.8,3.0,3.4,3.8,4.2,4.8};
+const int NdEtaBins = sizeof(dEtaBins) / sizeof(dEtaBins[0]) - 1;
+
+double get3Real(double R1, double R2, double R3, double I1, double I2, double I3){
+
+  double t1 = R1*R2*R3;
+  double t2 = R1*I2*I3;
+  double t3 = R2*I1*I3;
+  double t4 = I1*I2*R3;
+
+  return t1-t2-t3-t4;
+
+}
+double get3Imag(double R1, double R2, double R3, double I1, double I2, double I3){
+
+  double t1 = R1*R2*I3;
+  double t2 = R1*R3*I2;
+  double t3 = R2*R3*I1;
+  double t4 = I1*I2*I3;
+
+  return t1+t2+t3-t4;
+
+}
+double get2Real( double R1, double R2, double I1, double I2){
+
+	double real = R1*R2 - I1*I2;
+	return real;
+
+}
+double get2Imag( double R1, double R2, double I1, double I2){
+
+	double imag = R1*I2 + R2*I1;
+	return imag;
+}
+
+void plotEtaDependence(){
+
+
+	TFile* file = new TFile("../rootfiles/CME_QvsdEta_pPb_EPOS_etatest_v1.root");
+
+	TH1D* QvsdEta[1000][3][2];
+
+	TH1D* QaQb = (TH1D*) file->Get("ana/c2_ab");
+	TH1D* QaQc = (TH1D*) file->Get("ana/c2_ac");
+	TH1D* QcQb = (TH1D*) file->Get("ana/c2_cb");
+	TH1D* Ntrk = (TH1D*) file->Get("ana/Ntrk");
+
+	TH1D* aveQ3[2][2];
+
+	for(int i = 0; i < 2; i++){
+		for(int j = 0; j < 2; j++){
+
+			aveQ3[i][j] = (TH1D*)file->Get(Form("ana/aveQ3_%d_%d",i,j) );
+		}
+	}
+
+	for(int deta = 0; deta < Nbins; deta++){
+		for(int sign = 0; sign < 3; sign++){
+			for(int HF = 0; HF < 2; HF++){
+		  
+			  QvsdEta[deta][sign][HF] = (TH1D*) file->Get( Form("ana/QvsdEta_%d_%d_%d",deta,sign,HF) );
+			  
+			}
+		}
+	}
+
+	double meanQaQb = QaQb->GetMean();
+	double meanQaQc = QaQc->GetMean();
+	double meanQcQb = QcQb->GetMean();
+
+	double c2_a = meanQaQb*meanQaQc/meanQcQb;
+	double c2_b = meanQaQb*meanQcQb/meanQaQc;
+	double c2_ab = meanQaQb;
+
+	double bCorr = (aveQ3[0][0]->GetMean() * aveQ3[0][0]->GetMean()) +  ( aveQ3[0][1]->GetMean() * aveQ3[0][1]->GetMean() );
+	double aCorr = (aveQ3[1][0]->GetMean() * aveQ3[1][0]->GetMean()) +  ( aveQ3[1][1]->GetMean() * aveQ3[1][1]->GetMean() );
+	
+	double m1 = (aveQ3[0][0]->GetMean() + aveQ3[1][0]->GetMean())/2.0;
+	double m2 = (aveQ3[0][1]->GetMean() + aveQ3[1][1]->GetMean())/2.0;
+	double abCorr = m1*m1 + m2*m2;
+
+//correction factor for the V2 values from HF. 
+	double v2[3];
+	//v2[0] = sqrt(meanQcQb);
+	// v2[0] = sqrt(c2_b - bCorr);
+	// v2[1] = sqrt(c2_a - aCorr );
+	// v2[2] = sqrt(c2_ab - abCorr );
+
+	v2[0] = sqrt(c2_b );
+	v2[1] = sqrt(c2_a  );
+	v2[2] = sqrt(c2_ab );
+	cout << "v2[0]: " << v2[0] << endl;
+	cout << "v2[1]: " << v2[1] << endl;
+	cout << "v2[2]: " << v2[2] << endl;
+
+	TH1D* hist1[3][2];
+	TH1D* hist2[3][2];
+	for(int sign = 0; sign < 3; sign++){
+		for(int HF = 0; HF < 2; HF++){
+			hist1[sign][HF] = new TH1D(Form("hist1_%d_%d",sign,HF),"test", Nbins, etabins);
+			hist2[sign][HF] = new TH1D(Form("hist2_%d_%d",sign,HF),"test", Nbins, etabins);
+		}
+	}
+
+	for(int deta = 0; deta < Nbins; deta++){
+
+		for(int sign = 0; sign < 3; sign++){
+			for(int HF = 0; HF < 2; HF++){
+
+				double Q_total_real_dEta = QvsdEta[deta][sign][HF]->GetMean();
+				double Q_total_real_dEta_error = QvsdEta[deta][sign][HF]->GetMeanError();
+
+				hist1[sign][HF]->SetBinContent(deta+1, Q_total_real_dEta );
+				hist1[sign][HF]->SetBinError(deta+1,  Q_total_real_dEta_error);
+			}
+		}
+	}
+
+	TH1D* base3 = makeHist("base3","#delta#eta < 0.02","#eta", "cos(#phi_{1}+#phi_{2}-2#phi_{3})", 480,-2.4,2.4);
+    base3->GetXaxis()->SetTitleColor(kBlack);
+    base3->GetYaxis()->SetRangeUser(-0.0015,0.009);
+    base3->GetYaxis()->SetTitleOffset(1.9);
+
+    TH1D* base4 = (TH1D*) base3->Clone("base4");
+
+    TCanvas* c4 = makeMultiCanvas("c4","c4",2,1);
+    for(int HF = 0; HF < 2; HF++){
+		c4->cd(HF+1);
+		gPad->SetTicks();
+		gPad->SetLeftMargin(0.20);
+		gPad->SetBottomMargin(0.16);
+		
+		if(HF == 0) {base3->SetTitle("Pb-going");base3->Draw();}
+		if(HF == 1) {base4->SetTitle("p-going");base4->Draw();}
+
+		TH1D* temp = (TH1D*)hist1[1][HF]->Clone("temp");
+		//temp->Add(hist1[1][HF], +1);
+		//temp->Scale(0.5);
+
+		//temp->Scale( 1.0/v2[HF] );
+		temp->SetMarkerColor(kRed);
+		temp->SetLineColor(kRed);
+		temp->SetMarkerStyle(20);
+		temp->Draw("Psame");
+
+		TH1D* temp2 = (TH1D*)hist1[2][HF]->Clone("temp2");
+		//temp2->Scale( 1.0/v2[HF] );
+		temp2->SetMarkerColor(kBlue);
+		temp2->SetLineColor(kBlue);
+		temp2->SetMarkerStyle(24);
+		temp2->Draw("Psame");
+
+	}
+
+	TLegend *w2 = new TLegend(0.50,0.65,0.8,0.80);
+    w2->SetLineColor(kWhite);
+    w2->SetFillColor(0);
+    w2->SetTextSize(20);
+    w2->SetTextFont(43);
+    w2->AddEntry(temp, "like sign");
+    w2->AddEntry(temp2, "unlike sign");
+    w2->Draw("same");
+
+
+}
