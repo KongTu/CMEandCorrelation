eb874ca8d90e889799d703879888a3071155c729
Kong <kongPro@pb-d-128-141-167-56.cern.ch>
2016-05-09 16:48:13 +0200

    test EPOS eff

--- ThreePointCorrelator/src/ThreePointCorrelatorEtaGap.cc
+++ ThreePointCorrelator/src/ThreePointCorrelatorEtaGap.cc
@@ -461,7 +461,8 @@ ThreePointCorrelatorEtaGap::beginJob()
   int HFside = 2;
   if( useBothSide_ ) HFside = 1;
 
-  edm::FileInPath fip1("CMEandCorrelation/ThreePointCorrelator/data/TrackCorrections_HIJING_538_OFFICIAL_Mar24.root");
+  //edm::FileInPath fip1("CMEandCorrelation/ThreePointCorrelator/data/TrackCorrections_HIJING_538_OFFICIAL_Mar24.root");
+  edm::FileInPath fip1("CMEandCorrelation/ThreePointCorrelator/data/EPOS_eff.root");  
   TFile f1(fip1.fullPath().c_str(),"READ");
   effTable = (TH2D*)f1.Get("rTotalEff3D");
 
