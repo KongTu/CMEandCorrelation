b2d9716199558e0f9796ec0395d7684987a68195
Kong <kongPro@pb-d-128-141-151-13.cern.ch>
2016-05-19 09:28:43 +0200

    fix macros

--- ThreePointCorrelator/macros/plotDeltaEtaResult.C
+++ ThreePointCorrelator/macros/plotDeltaEtaResult.C
@@ -10,42 +10,21 @@ double dEtaBins[] = {0.0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0,1.1,1.2,1.3,1.4
 const int NdEtaBins = sizeof(dEtaBins) / sizeof(dEtaBins[0]) - 1;
 double ntrkBins[] = {0,35,60,90,120,150,185,220,260};
 const int NntrkBins = sizeof(ntrkBins) / sizeof(ntrkBins[0]) - 1;
-const int Nmults = 16;
+const int Nmults = 2;
 
-void plotIntegrated(){
+void plotDeltaEtaResult(){
 
 	TFile* file[16];
 
-	file[0] = new TFile("../rootfiles/CME_QvsdEta_pPb_MB_v4_1.root");
-	file[1] = new TFile("../rootfiles/CME_QvsdEta_pPb_MB_v4_2.root");
-	file[2] = new TFile("../rootfiles/CME_QvsdEta_pPb_MB_v4_3.root");
-	file[3] = new TFile("../rootfiles/CME_QvsdEta_pPb_MB_v4_4.root");
-	file[4] = new TFile("../rootfiles/CME_QvsdEta_pPb_HM_v32_1.root");
-	file[5] = new TFile("../rootfiles/CME_QvsdEta_pPb_HM_v32_2.root");
-	file[6] = new TFile("../rootfiles/CME_QvsdEta_pPb_HM_v32_3.root");
-	file[7] = new TFile("../rootfiles/CME_QvsdEta_pPb_HM_v32_4.root");
+	file[0] = new TFile("../rootfiles/CME_QvsdEta_pPb_HM_v32_3and4.root");
+	//file[0] = new TFile("../rootfiles/CME_QvsdEta_pPb_HM_v32_3.root");
 
-	file[8] = new TFile("../rootfiles/CME_QvsdEta_PbPb_50_100_v3_1.root");
-	file[9] = new TFile("../rootfiles/CME_QvsdEta_PbPb_50_100_v3_2.root");
-	file[10] = new TFile("../rootfiles/CME_QvsdEta_PbPb_50_100_v3_3.root");
-	file[11] = new TFile("../rootfiles/CME_QvsdEta_PbPb_50_100_v3_4.root");
-	file[12] = new TFile("../rootfiles/CME_QvsdEta_PbPb_50_100_v3_5.root");
-	file[13] = new TFile("../rootfiles/CME_QvsdEta_PbPb_50_100_v3_6.root");
-	file[14] = new TFile("../rootfiles/CME_QvsdEta_PbPb_50_100_v3_7.root");
-	file[15] = new TFile("../rootfiles/CME_QvsdEta_PbPb_50_100_v3_8.root");
+	file[1] = new TFile("../rootfiles/CME_QvsdEta_PbPb_50_100_v3_7and8.root");
+	// file[1] = new TFile("../rootfiles/CME_QvsdEta_PbPb_50_100_v3_7.root");
 
 	TH1D* QvsdEta[16][48][3][2];
 
 	TH1D* delEta3p[16][3][2];
-
-	for(int mult = 0; mult < Nmults; mult++){
-		for(int sign = 0; sign < 3; sign++){
-			for(int HF = 0; HF < 2; HF++){
-
-				delEta3p[mult][sign][HF] = (TH1D*) file[mult]->Get(Form("ana/delEta3p_%d_%d",sign,HF));
-			}
-		}
-	}
 
 	TH1D* QaQb[16]; TH1D* QaQc[16]; TH1D* QcQb[16];
 	TH1D* aveQ3[16][2][2];
@@ -63,7 +42,6 @@ void plotIntegrated(){
 			}
 		}
 	}
-
 
 
 	for(int mult = 0; mult < Nmults; mult++){
@@ -103,76 +81,41 @@ void plotIntegrated(){
 		v2[mult][2] = sqrt(c2_ab - abCorr );
 	}
 
-
-	TH1D* hist1[3][2];
-	TH1D* hist2[3][2];
-
-	for(int sign = 0; sign < 3; sign++){
-		for(int HF = 0; HF < 2; HF++){
-
-			hist1[sign][HF] = new TH1D(Form("hist1_%d_%d",sign,HF), "", NntrkBins, ntrkBins);
-			hist2[sign][HF] = new TH1D(Form("hist2_%d_%d",sign,HF), "", NntrkBins, ntrkBins);
-
-		}
-	}
-
-	double threeParticleNtrk[16][3][2];
-	double threeParticleNtrkError[16][3][2];
-	double totalWeight[16][3][2];
-
+	TH1D* hist1[8][3][2];
+	TH1D* hist2[8][3][2];
 	for(int mult = 0; mult < Nmults; mult++){
 		for(int sign = 0; sign < 3; sign++){
 			for(int HF = 0; HF < 2; HF++){
+				hist1[mult][sign][HF] = new TH1D(Form("hist1_%d_%d_%d",mult,sign,HF),"test", NdEtaBins, dEtaBins);
+				hist2[mult][sign][HF] = new TH1D(Form("hist2_%d_%d_%d",mult,sign,HF),"test", NdEtaBins, dEtaBins);
+			}
+		}
+	}
 
-				for(int deta = 0; deta < NdEtaBins; deta++){
+	for(int mult = 0; mult < Nmults; mult++){
+		for(int deta = 0; deta < NdEtaBins; deta++){
+			for(int sign = 0; sign < 3; sign++){
+				for(int HF = 0; HF < 2; HF++){
 
 					double Q_total_real_dEta = QvsdEta[mult][deta][sign][HF]->GetMean();
 					double Q_total_real_dEta_error = QvsdEta[mult][deta][sign][HF]->GetMeanError();
-					double deltaEtaWeight = delEta3p[mult][sign][HF]->GetBinContent( deta+1 );
-
-					threeParticleNtrk[mult][sign][HF] += Q_total_real_dEta*deltaEtaWeight;
-					threeParticleNtrkError[mult][sign][HF] += (Q_total_real_dEta_error*Q_total_real_dEta_error)*(deltaEtaWeight*deltaEtaWeight);
-					totalWeight[mult][sign][HF] += deltaEtaWeight;
-
+					
+					hist1[mult][sign][HF]->SetBinContent(deta+1, Q_total_real_dEta );
+					hist1[mult][sign][HF]->SetBinError(deta+1,  Q_total_real_dEta_error);
 				}
 			}
 		}
-
 	}
-
-	for(int sign = 0; sign < 3; sign++){
-		for(int HF = 0; HF < 2; HF++){
-			for(int mult = 0; mult < 8; mult++){
-
-				//pPb(0,7)
-				double value = threeParticleNtrk[mult][sign][HF]/totalWeight[mult][sign][HF];
-				value = value/v2[mult][HF];
-				hist1[sign][HF]->SetBinContent( mult+1, value);
-				double error = threeParticleNtrkError[mult][sign][HF]/(totalWeight[mult][sign][HF]*totalWeight[mult][sign][HF]);
-				hist1[sign][HF]->SetBinError( mult+1, sqrt(error));
-
-				//PbPb(8,15)
-				double value = threeParticleNtrk[mult+8][sign][HF]/totalWeight[mult+8][sign][HF];
-				value = value/v2[mult+8][HF];
-				hist2[sign][HF]->SetBinContent( mult+1, value);
-				double error = threeParticleNtrkError[mult+8][sign][HF]/(totalWeight[mult+8][sign][HF]*totalWeight[mult+8][sign][HF]);
-				hist2[sign][HF]->SetBinError( mult+1, sqrt(error));
-			}
-		}
-	}
-
-
-//start plotting
 
 	TGaxis::SetMaxDigits(3);
 
-	TH1D* base1 = makeHist("base1", "Pb-going", "N^{offline}_{trk}", "#LTcos(#phi_{#alpha}+#phi_{#beta}-2#Psi_{RP})#GT", 500,-8,280,kBlack);
-	TH1D* base2 = makeHist("base2", "p-going", "N^{offline}_{trk}", "#LTcos(#phi_{#alpha}+#phi_{#beta}-2#Psi_{RP})#GT", 500,-8,280,kBlack);
+	TH1D* base1 = makeHist("base1", "", "#Delta#eta", "#LTcos(#phi_{#alpha}+#phi_{#beta}-2#Psi_{RP})#GT", 48,0,4.8,kBlack);
+	TH1D* base2 = makeHist("base2", "", "#Delta#eta", "#LTcos(#phi_{#alpha}+#phi_{#beta}-2#Psi_{RP})#GT", 48,0,4.8,kBlack);
 
-	base1->GetYaxis()->SetRangeUser(-0.001, 0.015);
+	base1->GetYaxis()->SetRangeUser(-0.0014, 0.0018);
 	base1->GetXaxis()->SetTitleColor(kBlack);
 	
-	base2->GetYaxis()->SetRangeUser(-0.001, 0.015);
+	base2->GetYaxis()->SetRangeUser(-0.0014, 0.0018);
 	base2->GetXaxis()->SetTitleColor(kBlack);
 	
 	fixedFontHist1D(base1,1.1,1.25);
@@ -191,14 +134,15 @@ void plotIntegrated(){
 	base2->GetXaxis()->SetLabelSize(base2->GetXaxis()->GetLabelSize()*1.4);
 	
 	TH1D* base3 = (TH1D*) base1->Clone("base3");
-	base3->GetYaxis()->SetRangeUser(-0.0005,0.0013);
+	base3->GetYaxis()->SetTitle("#Delta#LTcos(#phi_{#alpha}+#phi_{#beta}-2#Psi_{RP})#GT");
+	base3->GetYaxis()->SetRangeUser(-0.001,0.002);
 	base3->GetYaxis()->SetTitleOffset(1.9);
 	base3->GetXaxis()->SetTitleOffset(3.1);
 	base3->GetYaxis()->SetTitleSize(base3->GetYaxis()->GetTitleSize()*1.0);
 	base3->GetYaxis()->SetNdivisions(6);
 	
 	TH1D* base4 = (TH1D*) base2->Clone("base4");
-	base4->GetYaxis()->SetRangeUser(-0.0005,0.0013);
+	base4->GetYaxis()->SetRangeUser(-0.001,0.002);
 	base4->GetYaxis()->SetTitleOffset(1.9);
 	base4->GetXaxis()->SetTitleOffset(3.1);
 	base4->GetYaxis()->SetTitleSize(base4->GetYaxis()->GetTitleSize()*1.0);
@@ -246,33 +190,37 @@ void plotIntegrated(){
 	gStyle->SetOptTitle(0);
 	base1->Draw();
 
-	TH1D* temp1 = (TH1D*)hist1[0][0]->Clone("temp1");
-	temp1->Add(hist1[1][0], +1);
+	TH1D* temp1 = (TH1D*)hist1[0][0][0]->Clone("temp1");
+	temp1->Add(hist1[0][1][0], +1);
 	temp1->Scale(0.5);
+	temp1->Scale(1.0/v2[0][0]);
 	temp1->SetMarkerStyle(24);
 	temp1->SetMarkerColor(kRed);
 	temp1->SetLineColor(kRed);
 
 	temp1->Draw("Psame");
 
-	TH1D* temp2 = (TH1D*) hist1[2][0]->Clone("temp2");
+	TH1D* temp2 = (TH1D*) hist1[0][2][0]->Clone("temp2");
 	temp2->SetMarkerStyle(25);
+	temp2->Scale(1.0/v2[0][0]);
 	temp2->SetMarkerColor(kBlue);
 	temp2->SetLineColor(kBlue);
 
 	temp2->Draw("Psame");
 
-	TH1D* temp3 = (TH1D*)hist1[0][1]->Clone("temp3");
-	temp3->Add(hist1[1][1], +1);
+	TH1D* temp3 = (TH1D*)hist1[0][0][1]->Clone("temp3");
+	temp3->Add(hist1[0][1][1], +1);
 	temp3->Scale(0.5);
+	temp3->Scale(1.0/v2[0][1]);
 	temp3->SetMarkerStyle(20);
 	temp3->SetMarkerColor(kRed);
 	temp3->SetLineColor(kRed);
 
 	temp3->Draw("Psame");
 
-	TH1D* temp4 = (TH1D*) hist1[2][1]->Clone("temp4");
+	TH1D* temp4 = (TH1D*) hist1[0][2][1]->Clone("temp4");
 	temp4->SetMarkerStyle(21);
+	temp4->Scale(1.0/v2[0][1]);	
 	temp4->SetMarkerColor(kBlue);
 	temp4->SetLineColor(kBlue);
 
@@ -285,7 +233,14 @@ void plotIntegrated(){
     r3->SetTextColor(kBlack);
     r3->Draw("same");
 
-	TLegend *w1 = new TLegend(0.55,0.4,0.7,0.7);
+    TLatex* lmult = new TLatex(0.6, 0.74, "185 #leq N^{offline}_{trk} < 260");
+    lmult->SetNDC();
+    lmult->SetTextSize(23);
+    lmult->SetTextFont(43);
+    lmult->SetTextColor(kBlack);
+    lmult->Draw("same");
+
+	TLegend *w1 = new TLegend(0.55,0.1,0.7,0.3);
     w1->SetLineColor(kWhite);
     w1->SetFillColor(0);
     w1->SetTextSize(20);
@@ -300,29 +255,41 @@ void plotIntegrated(){
 	pad2[1]->SetTicks();
 	base2->Draw();
 
-	TH1D* temp5 = (TH1D*)hist2[0][1]->Clone("temp5");
-	temp5->Add(hist2[1][1], +1);
-	temp5->Scale(0.5);
+	TH1D* temp5 = (TH1D*)hist1[1][0][0]->Clone("temp5");
+	TH1D* temp6 = (TH1D*)hist1[1][1][0]->Clone("temp6");
+	TH1D* temp7 = (TH1D*)hist1[1][0][1]->Clone("temp6");
+	TH1D* temp8 = (TH1D*)hist1[1][1][1]->Clone("temp6");
+
+	temp5->Add(temp6, +1);
+	temp5->Add(temp7, +1);
+	temp5->Add(temp8, +1);
+
+	temp5->Scale(0.25);
+	temp5->Scale(1.0/v2[1][2]);
 	temp5->SetMarkerStyle(24);
 	temp5->SetMarkerColor(kRed);
 	temp5->SetLineColor(kRed);
 
 	temp5->Draw("Psame");
 
-	TH1D* temp6 = (TH1D*) hist2[2][1]->Clone("temp6");
-	temp6->SetMarkerStyle(25);
-	temp6->SetMarkerColor(kBlue);
-	temp6->SetLineColor(kBlue);
+	TH1D* temp9 = (TH1D*) hist1[1][2][0]->Clone("temp9");
+	TH1D* temp10 = (TH1D*) hist1[1][2][1]->Clone("temp10");
+	temp9->Add(temp10, +1);
+	temp9->Scale(0.5);
+	temp9->Scale(1.0/v2[1][2]);
+	temp9->SetMarkerStyle(25);
+	temp9->SetMarkerColor(kBlue);
+	temp9->SetLineColor(kBlue);
 
-	temp6->Draw("Psame");
+	temp9->Draw("Psame");
 
-	TLegend *w2 = new TLegend(0.55,0.4,0.7,0.7);
+	TLegend *w2 = new TLegend(0.55,0.1,0.7,0.3);
     w2->SetLineColor(kWhite);
     w2->SetFillColor(0);
     w2->SetTextSize(20);
     w2->SetTextFont(43);
     w2->AddEntry(temp5, "like sign");
-    w2->AddEntry(temp6, "unlike sign");
+    w2->AddEntry(temp9, "unlike sign");
     w2->Draw("same");
 
    	TLatex* r1 = new TLatex(0.56,0.92, "CMS");
@@ -341,22 +308,40 @@ void plotIntegrated(){
     r4->SetTextSize(23);
     r4->SetTextFont(43);
     r4->SetTextColor(kBlack);
-    r4->Draw("same");
+    r4->Draw("same");	
 
 	pad2[2]->cd();
 	pad2[2]->SetTicks();
 	base3->Draw();
 
-	temp1->Draw("Psame");
-	temp2->Draw("Psame");
-	temp3->Draw("Psame");
-	temp4->Draw("Psame");
+	TH1D* diff1 = (TH1D*) temp2->Clone("diff1");
+	diff1->Add(temp1, -1);
+	
+	TH1D* diff2 = (TH1D*) temp4->Clone("diff2");
+	diff2->Add(temp3, -1);
+
+	TLegend *w3 = new TLegend(0.55,0.65,0.7,0.8);
+    w3->SetLineColor(kWhite);
+    w3->SetFillColor(0);
+    w3->SetTextSize(20);
+    w3->SetTextFont(43);
+    w3->AddEntry(diff1, "unlike-like sign, Pb-going");
+    w3->AddEntry(diff2, "unlike-like sign, p-going");
+    w3->Draw("same");
+
+
+	diff1->Draw("Psame");
+	diff2->Draw("Psame");
 
 	pad2[3]->cd();
 	pad2[3]->SetTicks();
 	base4->Draw();
-	temp5->Draw("Psame");
-	temp6->Draw("Psame");
+
+	TH1D* diff3 = (TH1D*) temp9->Clone("diff3");
+	diff3->Add(temp5, -1);
+
+	diff3->Draw("Psame");
+
 
 	return;
 
