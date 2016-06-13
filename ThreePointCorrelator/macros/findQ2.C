f03a66da45b092026a79725dedf32cfa8b5a3074
Kong <kongPro@Kongs-MacBook-Pro.local>
2016-06-13 17:56:55 +0200

    add multiplicity eff table

--- ThreePointCorrelator/macros/findQ2.C
+++ ThreePointCorrelator/macros/findQ2.C
@@ -5,9 +5,9 @@ using namespace std;
 void findQ2(){
 
 
-	TFile* file = new TFile("../rootfiles/CME_QvsdEta_pPb_HM_Q2_v1.root");
-	TH1D* q2_mag = (TH1D*) file->Get("ana/q2_mag");
-
+	TFile* file = new TFile("../rootfiles/CME_QvsdEta_pPb_Q2_v2_1.root");
+	TH2D* q2_tracker_HF = (TH2D*) file->Get("ana/q2_tracker_HF");
+	TH1D* q2_mag = (TH1D*) q2_tracker_HF->ProjectionY("q2_mag", 1,1000);
 
 	double total = q2_mag->Integral();
 	for(int i = 0; i < q2_mag->GetNbinsX(); i++){
@@ -16,7 +16,7 @@ void findQ2(){
 
 		double ratio = num/total;
 
-		if( ratio < 1.0 && ratio > 0.99){
+		if( ratio < 0.8 && ratio > 0.79){
 
 			cout << "bin number: " << i << endl;
 		}
